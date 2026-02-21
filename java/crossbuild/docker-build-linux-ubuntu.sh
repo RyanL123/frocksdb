eb550d3396f89a111b4cf898bd19db7ffaa0ac46
Ryan Li <ryan.lichangrui@gmail.com>
2026-02-21 04:29:10 +0000

    build on ubuntu 22.04

---
+++ java/crossbuild/docker-build-linux-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p /rocksdb-local-build
+rm -rf /rocksdb-local-build/*
+cp -r /rocksdb-host/* /rocksdb-local-build
+cd /rocksdb-local-build
+
+PORTABLE=1 USE_RTTI=1 make -j$(nproc) rocksdbjava
+
+cp java/target/librocksdbjni-linux*.so \
+   java/target/rocksdbjni-*-linux*.jar \
+   java/target/rocksdbjni-*-linux*.jar.sha1 \
+   /rocksdb-java-target/
